82fb62e5dac109695cd92b19bfb6b0747dde7000
Pitchayut-got <gotcan321@gmail.com>
2025-02-06 00:21:57 +0700

    ล่าสุด

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -203,8 +203,76 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+             Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                  childAspectRatio: 1.5,
+                  children: [
+                    buildCategoryTile(Icons.local_offer, "Sale"),
+                    buildCategoryTile(Icons.sports_soccer, "Sport"),
+                    buildCategoryTile(Icons.new_releases, "New & Trending"),
+                    buildCategoryTile(Icons.card_giftcard, "Gift Cards"),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'adidas',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+        Widget buildCategoryTile(IconData icon, String label) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            blurRadius: 5,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 40, color: Colors.black),
+          SizedBox(height: 14),
+          Text(
+            label,
+            style: GoogleFonts.lato(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
